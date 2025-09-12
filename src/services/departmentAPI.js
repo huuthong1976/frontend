ff3e580eaad2cec672c6608ebbf013ac59c7b95b
huuthong1976 <huuthong27061976@gmail.com>
2025-09-12 14:27:48 +0700

    Update departmentAPI.js

--- src/services/departmentAPI.js
+++ src/services/departmentAPI.js
@@ -3,20 +3,20 @@ import apiClient from './apiClient'; // Import axios instance đã cấu hình
 
 
 export const getDepartments = (params) => {
-    return apiClient.get('/departments', { params });
+    return apiClient.get('/api/departments', { params });
 };
 
 export const createDepartment = (departmentData) => {
-    return apiClient.post('/departments', departmentData);
+    return apiClient.post('/api/departments', departmentData);
 };
 
 export const updateDepartment = (id, departmentData) => {
-    return apiClient.put(`/departments/${id}`, departmentData);
+    return apiClient.put(`/api/departments/${id}`, departmentData);
 };
 
 export const deleteDepartment = (id) => {
-    return apiClient.delete(`/departments/${id}`);
+    return apiClient.delete(`/api/departments/${id}`);
 };
 export const getDepartmentById = (id) => {
-    return apiClient.get(`/departments/${id}`);
+    return apiClient.get(`/api/departments/${id}`);
   };
